7e2673b8498a7aabe86d248215479aa5c57a8d48
hemanth22 <hemanth22hemu@gmail.com>
2020-06-27 16:22:53 +0530

    feat: writing and running a test

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -8,12 +8,13 @@ namespace GradeBook.Tests
         [Fact]
         public void Test1()
         {
+            // arrange
             var x = 5;
             var y = 2;
             var expected = 7;
-
+            // act
             var actual = x + y;
-
+            // assert
             Assert.Equal(expected,actual);
 
         }
