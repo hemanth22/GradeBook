60b80708173da6414ac60dec29e0ef6978cf9f80
hemanth22 <hemanth22hemu@gmail.com>
2020-06-27 15:38:48 +0530

    add src code

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Reflection.Metadata;
+using System.Threading.Tasks.Dataflow;
+using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Bitra's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
+        }
+    }
+}
