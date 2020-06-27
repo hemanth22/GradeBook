1781606f1e6664b136d4beccf9e614b364be0050
hemanth22 <hemanth22hemu@gmail.com>
2020-06-27 20:23:16 +0530

    test: adding different type of tests

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,70 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void ValueTypesAlsoPassByValue()
+        {
+        // Arrange
+            var x = GetInt();      
+        // Act
+            SetInt(ref x);
+        // Assert
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int z)
+        {
+            z = 42;
+        }
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(out book1, "New Name");
+
+            Assert.Equal("New Name",book1.Name);
+        }
+//ref or out for passing paramters by reference
+// out will throw error if it is not initialized 
+        private void GetBookSetName(out Book book,string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1",book1.Name);
+        }
+
+        private void GetBookSetName(Book book,string name)
+        {
+            book = new Book(name);
+        }
+
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name",book1.Name);
+        }
+
+        private void SetName(Book book,string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
         public void GetBookReturnsDifferentObject()
         {
             var book1 = GetBook("Book 1");
