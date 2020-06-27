2ca17c06f0011a5b3d1f672306dcd591c1dddcd4
hemanth22 <hemanth22hemu@gmail.com>
2020-06-27 15:39:51 +0530

    src/Program

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -12,7 +12,7 @@ namespace GradeBook
             var book = new Book("Bitra's Grade Book");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            book.AddGrade(77.5;
             book.ShowStatistics();
         }
     }
